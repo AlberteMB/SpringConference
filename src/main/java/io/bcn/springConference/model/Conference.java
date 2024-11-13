49e5da47cad0616eb6fdf07771d07c4841940e49
AlberteMB <albertebardanca@hotmail.com>
2024-11-13 19:48:32 +0100

    New models: Speaker and Conference.

--- src/main/java/io/bcn/springConference/model/Conference.java
+++ src/main/java/io/bcn/springConference/model/Conference.java
@@ -1,4 +1,21 @@
 package io.bcn.springConference.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
 public class Conference {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDate date;
+
+    @OneToMany(mappedBy = "conference")
+    private List<Speaker> speakers;
+    // Getters and setters
 }
--- src/main/java/io/bcn/springConference/model/Speaker.java
+++ src/main/java/io/bcn/springConference/model/Speaker.java
@@ -1,4 +1,18 @@
 package io.bcn.springConference.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class Speaker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "conference_id")
+    private Conference conference;
+    // Getters and setters
 }
