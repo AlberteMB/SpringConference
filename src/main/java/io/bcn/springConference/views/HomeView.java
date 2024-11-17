98404943d9b689f71ed10906259c2ba3ba1b3f0b
AlberteMB <albertebardanca@hotmail.com>
2024-11-17 20:25:33 +0100

    Adding avatar in homeView. CSS for Avatar.

--- src/main/java/io/bcn/springConference/views/HomeView.java
+++ src/main/java/io/bcn/springConference/views/HomeView.java
@@ -1,32 +1,54 @@
 package io.bcn.springConference.views;
 
 import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.avatar.Avatar;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
 import io.bcn.springConference.view.ConferenceView;
 import io.bcn.springConference.view.SpeakerView;
+
+import javax.swing.*;
+
 @PageTitle("Main Layout")
 @Route("")
 public class HomeView extends AppLayout {
 
     public HomeView() {
+        // Avatar
+        Avatar avatar = new Avatar("Alberte Martínez");
+        avatar.setImage("avatar.png"); //
+        avatar.setHeight("50px");
+        avatar.setWidth("50px");
 
-        // Logo
-        Image logo = new Image("logo.png", "My Logo");
-        logo.setHeight("50px");
-        addToNavbar(logo);
 
-        // Navigation
-        addToDrawer(new RouterLink("Conferences", ConferenceView.class));
-        addToDrawer(new RouterLink("Speakers", SpeakerView.class));
+        // Layout para logo y avatar
+        HorizontalLayout navbarLayout = new HorizontalLayout(avatar);
+        avatar.addClassName("avatar");
+        navbarLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        navbarLayout.setSpacing(true);
 
-        // Pie de página o elementos adicionales
+        addToNavbar(navbarLayout);
+
+
+        RouterLink conferencesLink = new RouterLink("Conferences", ConferenceView.class);
+        conferencesLink.addClassName("drawer-link");
+
+        RouterLink speakersLink = new RouterLink("Speakers", SpeakerView.class);
+        speakersLink.addClassName("drawer-link");
+
+        addToDrawer(conferencesLink, speakersLink);
+
+        HorizontalLayout spaceLayout = new HorizontalLayout();
+        spaceLayout.setWidth("20px");
+        spaceLayout.setHeight("20px");
+
+        addToNavbar(spaceLayout);
+
         addToNavbar(new Paragraph("Main page"));
-
     }
 }
